cc902e0d72e269958ef80984a7bfc7d26acc8935
Mai Vo Van <maivv@nal.vn>
2024-09-26 17:31:45 +0700

    refactor

--- src/app/dashboard/roles/Search.tsx
+++ src/app/dashboard/roles/Search.tsx
@@ -34,7 +34,7 @@ const Search = ({ formData, onSearch, resetForm }: PropsType) => {
         updated_at_to: formData.updated_at_to ? dayjs(formData.updated_at_to) : null,
       });
     }
-  }, [formData]);
+  }, [formData, form]);
 
   const formatDates = (values: any) => {
     ['created_at_from', 'created_at_to', 'updated_at_from', 'updated_at_to'].forEach((field) => {
