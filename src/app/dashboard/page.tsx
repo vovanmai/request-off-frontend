766cf0a53ed641ade5cf6c6cfdaf15a27a7dd4ce
Mai Vo Van <maivv@nal.vn>
2024-10-02 16:14:40 +0700

    test scroll x

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,47 @@
-'use client'
 import React from 'react';
+import { Table } from 'antd';
+import type { TableColumnsType } from 'antd';
 
-const Home = () => {
-  return (
-    <div>
-      This is dashboard page
-    </div>
-  );
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
 }
 
-export default Home
+const columns: TableColumnsType<DataType> = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    width: 150,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    width: 150,
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+  },
+];
+
+const dataSource = Array.from({ length: 100 }).map<DataType>((_, i) => ({
+  key: i,
+  name: `Edward King ${i}`,
+  age: 32,
+  address: `London, Park Lane no. ${i}`,
+}));
+
+const App: React.FC = () => {
+  return (
+    <Table<DataType>
+      columns={columns}
+      dataSource={dataSource}
+      pagination={{ pageSize: 50 }}
+      scroll={{x: 'max-content', y: 55 * 5 }}
+    />
+  );
+};
+
+export default App;
