d1e6dd642937a690c12321a0208911ccd0b936e1
Mai Vo Van <maivv@nal.vn>
2024-10-02 16:22:57 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,47 @@
-'use client'
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import SpinLoading from "../components/SpinLoading";
+import React from 'react';
+import { Table } from 'antd';
+import type { TableColumnsType } from 'antd';
 
-export default function Home() {
-  const router = useRouter()
-  useEffect(() => {
-    router.push('/dashboard')
-  }, [router])
-
-  return (
-    <SpinLoading/>
-  );
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
 }
+
+const columns: TableColumnsType<DataType> = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    width: 150,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    width: 150,
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+  },
+];
+
+const dataSource = Array.from({ length: 100 }).map<DataType>((_, i) => ({
+  key: i,
+  name: `Edward King ${i}`,
+  age: 32,
+  address: `London, Park Lane no. ${i}`,
+}));
+
+const App: React.FC = () => {
+  return (
+    <Table<DataType>
+      columns={columns}
+      dataSource={dataSource}
+      pagination={{ pageSize: 50 }}
+      scroll={{x: 'max-content', y: 55 * 5 }}
+    />
+  );
+};
+
+export default App;
